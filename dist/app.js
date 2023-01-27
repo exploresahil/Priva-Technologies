4e8fc4332d75f27da6a1fc4c702490d2b4b7295b
Sahil Satpute <explore.sahil@outlook.com>
2023-01-27 15:54:46 +0530

    Updated JS

--- dist/app.js
+++ dist/app.js
@@ -24,10 +24,40 @@ backspaceButton.addEventListener("click", function () {
   }
 });
 
+const mobNoSuccess = document.querySelector("#mobNoSuccess");
+
+const mobNoSuccessST = new SplitType("#mobNoSuccess", {
+  charClass: "mobNoSuccessChar",
+});
+
 confirmButton.addEventListener("click", function () {
   if (phoneNumber.value.length === 13) {
-    // Do something with the phone number
-    window.location.href = "../index.html";  
+    mobNoSuccess.classList.remove("hidden");
+    mainReady.classList.add("visibilityHidden");
+
+    gsap.from(".mobNoSuccessChar", {
+      y: 80,
+      opacity: "0",
+      stagger: 0.02,
+      duration: 0.05,
+      ease: Power1.easeOut,
+    });
+
+    gsap.to(".mobNoSuccessChar", {
+      y: -80,
+      opacity: "0",
+      stagger: 0.02,
+      duration: 0.05,
+      ease: Power1.easeOut,
+
+      delay: 1.2,
+    });
+
+    setTimeout(function () {
+      mobNoSuccess.classList.add("hidden");
+      mainReady.classList.remove("visibilityHidden");
+      window.location.href = "../index.html";
+    }, 1600);
   }
 });
 
@@ -37,7 +67,6 @@ clearButton.addEventListener("click", function () {
 
 //*----------> Home click function
 const home = document.querySelector("#home");
-
 home.addEventListener("click", function () {
   window.location.href = "../index.html";
 });
